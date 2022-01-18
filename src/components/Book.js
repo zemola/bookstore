507398b7a7a4c242e9296f6f82f10910111c48b6
unknown <azeemolatunji@gmail.com>
2022-01-18 22:39:12 +0100

    Made changes as requested by reviwer

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,25 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Book = (props) => {
-  const name = props;
-  return (
-    <div key={name.data[0].id}>
-      <h1>{name.data[0].type}</h1>
-      <h2>{name.data[0].title}</h2>
-      <h2>{name.data[0].author}</h2>
-      <Button />
-      <h1>{name.data[1].type}</h1>
-      <h2>{name.data[1].title}</h2>
-      <h2>{name.data[1].author}</h2>
-      <Button />
-      <h1>{name.data[2].type}</h1>
-      <h2>{name.data[2].title}</h2>
-      <h2>{name.data[2].author}</h2>
-      <Button />
+const Book = ({ data }) => (
+  <div>
+    {data.map((book) => (
+      <>
+        <h1>{book.type}</h1>
+        <h2>{book.title}</h2>
+        <h2>{book.author}</h2>
+        <Button />
+      </>
+    ))}
+  </div>
+);
 
-    </div>
-  );
+Book.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default Book;
