a9f2d448c406719bbd6725e2c1630d8070a5c53d
unknown <azeemolatunji@gmail.com>
2022-01-20 08:45:31 +0100

    Create branch for Api, apply Middleware for Api functionality and thunk

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,10 +1,12 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const FETCH_BOOK = 'bookStore/books/FETCH_BOOK';
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/7BNFfUW0dAlw3zogd5io/books/';
 const initialState = [];
 
 export const addBook = (payload) => ({
   type: ADD_BOOK,
-  payload,
+  payload: { ...payload },
 });
 
 export const removeBook = (payload) => ({
@@ -12,14 +14,54 @@ export const removeBook = (payload) => ({
   payload,
 });
 
+export const getBook = (payload) => ({
+  type: FETCH_BOOK,
+  payload,
+});
+
+export const addBookToApi = (payload) => async (dispatch) => {
+  await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/7BNFfUW0dAlw3zogd5io/books', {
+    method: 'POST',
+    body: JSON.stringify(payload),
+    headers: { 'Content-Type': 'application/json' },
+  }).then((result) => {
+    if (result.status === 201) {
+      dispatch(addBook(payload));
+    }
+  });
+};
+
+export const removeBookFromApi = (payload) => async (dispatch) => {
+  await fetch(`${url}${payload}`, {
+    method: 'DELETE',
+    body: JSON.stringify(),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  dispatch(removeBook(payload));
+};
+
+export const getBookFromApi = () => async (dispatch) => {
+  const request = await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/7BNFfUW0dAlw3zogd5io/books');
+  const response = await request.json();
+  dispatch(getBook(response));
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload.id);// eslint-disable-line
-    default:
-      return state;
+      return state.filter((book) => book.item_id !== action.payload);// eslint-disable-line
+      case FETCH_BOOK:
+        return Object.entries(action.payload).map(([key, value]) => {
+          const [book] = value;
+          return {
+            item_id: key,
+            ...book,
+          };
+        });
+      default:
+        return state;
   }
 };
 
